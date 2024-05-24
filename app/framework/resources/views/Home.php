28aacc48c8e0515c7ab7254c399aa9fad3592eac
ShinyPineMaker <guizin.lps@gmail.com>
2024-05-24 13:18:01 -0300

    Adding the icons and png
    dummy home.php with link to logo icon
    adding files to assets/img

--- app/framework/resources/views/Home.php
+++ app/framework/resources/views/Home.php
@@ -1,2 +1,13 @@
-<h1>Hello World!</h1>
-<p>Welcome <?php echo $name; ?></p>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="public/assets/imgs/spothlight-logo.ico" type="image/x-icon">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
